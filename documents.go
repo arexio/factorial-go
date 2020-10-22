7de8563a5219cdb2e0f05cf6a50fdd3f93bb96a9
Ramon Macias <ramonmaciasgarcia@gmail.com>
2020-10-22 12:00:42 +0200

    feat :sparkles:: Added endpoints for Document service

--- documents.go
+++ documents.go
@@ -23,6 +23,28 @@ type Document struct {
 	UpdatedAt  string `json:"updated_at"`
 }
 
+// CreateDocumentRequest will hold the basic information
+// needed for create a new document in Factorial
+type CreateDocumentRequest struct {
+	Public            bool   `json:"public"`
+	EmployeeID        int    `json:"employee_id"`
+	File              string `json:"file"`
+	FileName          string `json:"filename"`
+	FolderID          int    `json:"folder_id"`
+	RequestESignature bool   `json:"request_esignature"`
+	Signees           []int  `json:"signees"`
+}
+
+// UpdateDocumentRequest will hold the basic information
+// for update a given document
+type UpdateDocumentRequest struct {
+	Public            bool  `json:"public"`
+	EmployeeID        int   `json:"employee_id"`
+	FolderID          int   `json:"folder_id"`
+	RequestESignature bool  `json:"request_esignature"`
+	Signees           []int `json:"signees"`
+}
+
 // ListDocuments gets all the documents from your company
 // you can filter this list by employee_id and folder_id
 func (c Client) ListDocuments(filter url.Values) ([]Document, error) {
@@ -39,4 +61,64 @@ func (c Client) ListDocuments(filter url.Values) ([]Document, error) {
 	}
 
 	return documents, nil
+}
+
+// GetDocument return the document saved in Factorial with
+// the given id
+func (c Client) GetDocument(id string) (Document, error) {
+	var document Document
+
+	resp, err := c.get(documentURL+"/"+id, nil)
+	if err != nil {
+		return document, err
+	}
+	defer resp.Body.Close()
+
+	if err := json.NewDecoder(resp.Body).Decode(&document); err != nil {
+		return document, err
+	}
+
+	return document, nil
+}
+
+// CreateDocument creates a new document in Factorial
+func (c Client) CreateDocument(d CreateDocumentRequest) (Document, error) {
+	var document Document
+
+	bytes, err := json.Marshal(d)
+	if err != nil {
+		return document, err
+	}
+
+	resp, err := c.post(documentURL, bytes)
+	if err != nil {
+		return document, err
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&document); err != nil {
+		return document, err
+	}
+
+	return document, nil
+}
+
+// UpdateDocument update the given document id with the given data
+func (c Client) UpdateDocument(id string, d UpdateDocumentRequest) (Document, error) {
+	var document Document
+
+	bytes, err := json.Marshal(d)
+	if err != nil {
+		return document, err
+	}
+
+	resp, err := c.put(documentURL+"/"+id, bytes)
+	if err != nil {
+		return document, err
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&document); err != nil {
+		return document, err
+	}
+
+	return document, nil
 }
